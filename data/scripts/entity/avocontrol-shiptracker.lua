bf8d40de941e74724384e4b315e57b2092e99960
Andrew Wyatt <sleepyfugu@gmail.com>
2020-12-05 01:27:52 -0500

    Tracking now kicks Alliance members

--- data/scripts/entity/avocontrol-shiptracker.lua
+++ data/scripts/entity/avocontrol-shiptracker.lua
@@ -17,17 +17,17 @@
 -- modified to utilize a better method of handling later on.
 local known_unique = {}
 local basepath = 'data/scripts/systems/'
-known_unique[basepath..'resistancesystem.lua'] = true
-known_unique[basepath..'teleporterkey1.lua'] = true
-known_unique[basepath..'teleporterkey2.lua'] = true
-known_unique[basepath..'teleporterkey3.lua'] = true
-known_unique[basepath..'teleporterkey4.lua'] = true
-known_unique[basepath..'teleporterkey5.lua'] = true
-known_unique[basepath..'teleporterkey6.lua'] = true
-known_unique[basepath..'teleporterkey7.lua'] = true
-known_unique[basepath..'teleporterkey8.lua'] = true
+known_unique[basepath..'resistancesystem.lua']  = true
+known_unique[basepath..'teleporterkey1.lua']    = true
+known_unique[basepath..'teleporterkey2.lua']    = true
+known_unique[basepath..'teleporterkey3.lua']    = true
+known_unique[basepath..'teleporterkey4.lua']    = true
+known_unique[basepath..'teleporterkey5.lua']    = true
+known_unique[basepath..'teleporterkey6.lua']    = true
+known_unique[basepath..'teleporterkey7.lua']    = true
+known_unique[basepath..'teleporterkey8.lua']    = true
 known_unique[basepath..'valuablesdetector.lua'] = true
-known_unique[basepath..'weaknesssystem.lua'] = true
+known_unique[basepath..'weaknesssystem.lua']    = true
 function known_unique.__newindex()
   return false
 end
@@ -72,12 +72,29 @@ function AvorionControlShipTracker.updateServer(...)
 
   if using_stack_exploit then
     if e.allianceOwned then
+      if messages_sent < messages_till_kick then
+        messages_sent = messages_sent + 1
+        print("Messages till kick: "..tostring(messages_till_kick - messages_sent))
+      else
+        local server = Server()
+        for _, member in pairs(Alliance(e.factionIndex)) do
+          if server:isOnline(member.factionIndex) then
+            print("doPlayerKickEvent: ${p} ${r}"%_T % {
+              p=member.factionIndex,
+              r="(Alliance) Using known exploits despite being warned"})
+          end
+        end
+      end
+
       print("allianceExploitingStackedSystemsEvent: alliance:${i} ${n}"%_T % {
         i=tostring(e.factionIndex),
         n=tostring(e.name)})
 
       Alliance(e.factionIndex):sendChatMessage("Server", ChatMessageType.Error,
-        "The ship \"${n}\" currently has multiple unique artifacts of the same type installed. This has been logged and the admins have been notified. You will continue to get this message until they are removed."%_T % {n=tostring(e.name)})
+        "The ship \"${n}\" currently has multiple unique artifacts of the same type installed.\n"%_T % {
+          n=tostring(e.name)}..
+        "This has been logged and the admins have been notified.\n"..
+        "You will continue to get this message until they are removed, or you are auto-kicked.")
       return
     end
 
@@ -96,7 +113,10 @@ function AvorionControlShipTracker.updateServer(...)
         n=tostring(e.name)})
 
       Player(e.factionIndex):sendChatMessage("Server", ChatMessageType.Error,
-        "The ship \"${n}\" currently has multiple unique artifacts of the same type installed. This has been logged and the admins have been notified. You will continue to get this message until they are removed, or you are auto-kicked."%_T % {n=tostring(e.name)}) 
+        "The ship \"${n}\" currently has multiple unique artifacts of the same type installed.\n"%_T % {
+          n=tostring(e.name)}..
+        "This has been logged and the admins have been notified.\n"..
+        "You will continue to get this message until they are removed, or you are auto-kicked.")
     end
   end
 end
