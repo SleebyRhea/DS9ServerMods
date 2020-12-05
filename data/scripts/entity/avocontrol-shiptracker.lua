5236c0bfc382439774dfa8ada70031b1ca465e38
Andrew Wyatt <sleepyfugu@gmail.com>
2020-12-04 22:22:54 -0500

    Add kick event output

--- data/scripts/entity/avocontrol-shiptracker.lua
+++ data/scripts/entity/avocontrol-shiptracker.lua
@@ -17,7 +17,6 @@
 -- modified to utilize a better method of handling later on.
 local known_unique = {}
 local basepath = 'data/scripts/systems/'
-  
 known_unique[basepath..'resistancesystem.lua'] = true
 known_unique[basepath..'teleporterkey1.lua'] = true
 known_unique[basepath..'teleporterkey2.lua'] = true
@@ -29,14 +28,20 @@ known_unique[basepath..'teleporterkey7.lua'] = true
 known_unique[basepath..'teleporterkey8.lua'] = true
 known_unique[basepath..'valuablesdetector.lua'] = true
 known_unique[basepath..'weaknesssystem.lua'] = true
-
 function known_unique.__newindex()
   return false
 end
 
+local messages_till_kick = 10
+local messages_sent      = 0
+
+-- AvorionControlShipTracker.getUpdateInterval adds an update interval if it
+--  doesn't already exist. In addition, this only occurs on the server.
 if type(AvorionControlShipTracker.getUpdateInterval) ~= "function" then
-  function AvorionControlShipTracker.getUpdateInterval()
-    return 30
+  if onServer() then
+    function AvorionControlShipTracker.getUpdateInterval()
+      return 30
+    end
   end
 end
 
@@ -72,12 +77,22 @@ function AvorionControlShipTracker.updateServer(...)
     end
 
     if e.playerOwned then
+      if Server():isOnline(e.factionIndex) then
+        if messages_sent < messages_till_kick then
+          messages_sent = messages_sent + 1
+        else
+          print("doPlayerKickEvent: ${p} ${r}"%_T % {
+            p=e.factionIndex,
+            r="Using known exploits despite being warned"})
+        end
+      end
+
       print("playerExploitingStackedSystemsEvent: player:${i} ${n}"%_T % {
         i=tostring(e.factionIndex),
         n=tostring(e.name)})
 
       Player(e.factionIndex):sendChatMessage("Server", ChatMessageType.Error,
-        "The ship \"${n}\" currently has multiple unique artifacts of the same type installed. This has been logged and the admins have been notified. You will continue to get this message until they are removed, or you are auto-kicked."%_T % {n=tostring(e.name)})
+        "The ship \"${n}\" currently has multiple unique artifacts of the same type installed. This has been logged and the admins have been notified. You will continue to get this message until they are removed, or you are auto-kicked."%_T % {n=tostring(e.name)}) 
     end
   end
 end
