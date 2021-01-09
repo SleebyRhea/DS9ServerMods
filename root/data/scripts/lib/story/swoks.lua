3e13b70f650e9dd41239542d5a6cd696feaf4413
Andrew Wyatt <sleepyfugu@gmail.com>
2021-01-09 18:17:20 -0500

    Remove unneeded call to Sector()

--- root/data/scripts/lib/story/swoks.lua
+++ root/data/scripts/lib/story/swoks.lua
@@ -2,6 +2,5 @@
 local oldSwoks_spawn = Swoks.spawn
 function Swoks.spawn(player, x, y)
   oldSwoks_spawn(player, x, y)
-  local x, y = Sector():getCoordinates()
   print("bossSpawnEvent: ${x}:${y} Boss Swoks"%_T % {x=tostring(x), y=tostring(y)})
 end
